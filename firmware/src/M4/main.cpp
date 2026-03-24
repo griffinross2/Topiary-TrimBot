d6ac3aa0f6f287e2ed7b7df6409fd099f47b81c9
griffinross2 <griffinross2@gmail.com>
2026-03-24 13:50:33 -0400

    Start preparing actual project firmware directory

---
+++ firmware/src/M4/main.cpp
@@ -0,0 +1,19 @@
+#include "stm32h7xx_hal.h"
+
+#include <stdio.h>
+
+extern "C" int main(void);
+
+int main(void)
+{
+    HAL_Init();
+
+    // Main loop
+    while (1)
+    {
+        printf("Hello from M4 core!\n");
+        HAL_Delay(1000);
+    }
+
+    return 0;
+}
