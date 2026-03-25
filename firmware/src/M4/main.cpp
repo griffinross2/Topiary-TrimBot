30e9704aeb557d845106e4c4bda52fded12a397f
griffinross2 <griffinross2@gmail.com>
2026-03-25 12:03:21 -0400

    More changes in the hope of blinky

--- firmware/src/M4/main.cpp
+++ firmware/src/M4/main.cpp
@@ -2,8 +2,6 @@
 
 #include <stdio.h>
 
-// extern "C" int main(void);
-
 int main(void)
 {
     HAL_Init();
@@ -15,4 +13,63 @@ int main(void)
     }
 
     return 0;
+}
+
+extern "C"
+{
+    void NMI_Handler(void);
+    void HardFault_Handler(void);
+    void MemManage_Handler(void);
+    void BusFault_Handler(void);
+    void UsageFault_Handler(void);
+    void SVC_Handler(void);
+    void DebugMon_Handler(void);
+    void PendSV_Handler(void);
+    void SysTick_Handler(void);
+}
+
+void NMI_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void MemManage_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void BusFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void UsageFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+void SVC_Handler(void) {}
+
+void DebugMon_Handler(void) {}
+
+void PendSV_Handler(void) {}
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
 }
--- firmware/src/M7/main.cpp
+++ firmware/src/M7/main.cpp
@@ -2,54 +2,18 @@
 
 #include <stdio.h>
 
-// extern "C" int main(void);
-
 int main(void)
 {
     HAL_Init();
 
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    /** Supply configuration update enable
-     */
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
     HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /** Configure the main internal regulator output voltage
-     */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY))
+    {
+    }
 
-    while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
-    // /** Initializes the RCC Oscillators according to the specified parameters
-    //  * in the RCC_OscInitTypeDef structure.
-    //  */
-    // RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    // RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-    // RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    // RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-    // if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    // {
-    //     while(1);
-    // }
-
-    // /** Initializes the CPU, AHB and APB buses clocks
-    //  */
-    // RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-    //                             |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-    //                             |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-    // RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    // RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-    // RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
-    // RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-    // RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-    // RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-    // RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
-
-    // if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-    // {
-    //     while(1);
-    // } 
     __HAL_RCC_GPIOD_CLK_ENABLE();
 
     GPIO_InitTypeDef gpioConf = {
@@ -74,35 +38,61 @@ int main(void)
     return 0;
 }
 
-extern "C" {
+extern "C"
+{
     void NMI_Handler(void);
     void HardFault_Handler(void);
     void MemManage_Handler(void);
     void BusFault_Handler(void);
     void UsageFault_Handler(void);
+    void SVC_Handler(void);
+    void DebugMon_Handler(void);
+    void PendSV_Handler(void);
     void SysTick_Handler(void);
 }
 
-void NMI_Handler(void) {
-    while(1) {}
+void NMI_Handler(void)
+{
+    while (1)
+    {
+    }
 }
 
-void HardFault_Handler(void) {
-    while(1) {}
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+    }
 }
 
-void MemManage_Handler(void) {
-    while(1) {}
+void MemManage_Handler(void)
+{
+    while (1)
+    {
+    }
 }
 
-void BusFault_Handler(void) {
-    while(1) {}
+void BusFault_Handler(void)
+{
+    while (1)
+    {
+    }
 }
 
-void UsageFault_Handler(void) {
-    while(1) {}
+void UsageFault_Handler(void)
+{
+    while (1)
+    {
+    }
 }
 
-void SysTick_Handler(void) {
+void SVC_Handler(void) {}
+
+void DebugMon_Handler(void) {}
+
+void PendSV_Handler(void) {}
+
+void SysTick_Handler(void)
+{
     HAL_IncTick();
 }
