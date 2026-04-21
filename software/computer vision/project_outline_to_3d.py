ef578ae7ee172371df4240de4f2cd242ecde3701
Jackson-Duke <duke10@purdue.edu>
2026-04-21 01:29:18 -0400

    rotated 3d plotting

--- software/computer vision/project_outline_to_3d.py
+++ software/computer vision/project_outline_to_3d.py
@@ -7,7 +7,7 @@ def project_outline_to_3d(outline_points, camera_orig_dist, disp, depth, angle, 
     r = camera_orig_dist - depth
 
     # pick basis vectors
-    n = np.array([np.cos(np.radians(angle)), np.sin(np.radians(angle)), 0])
+    n = np.array([-1 * np.cos(np.radians(angle)),-1 * np.sin(np.radians(angle)), 0])
     u1 = (0, 0, 1)
     u2 = np.cross(n, u1)
     R = np.array([u2, u1, n]).T
@@ -25,7 +25,7 @@ def project_outline_to_3d(outline_points, camera_orig_dist, disp, depth, angle, 
         y_inch = y_offset * y_scale
 
         # Transform this cross section by placing it on its plane in 3D
-        pt = np.matmul(R, np.array([-1 * x_inch, y_inch, 0])) + (r * n)
+        pt = np.matmul(R, np.array([x_inch, y_inch, 0])) + (r * n)
         
         ## CONDITION TO DECREASE POINT SAMPLING:
         # if(i % 10 == 0): 
