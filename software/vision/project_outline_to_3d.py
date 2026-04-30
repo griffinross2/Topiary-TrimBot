ca646c248896e59ab7d8a7156252defd19dc011f
Griffin Ross <griffinross2@gmail.com>
2026-04-30 00:56:42 -0400

    ts is NOT good

--- software/vision/project_outline_to_3d.py
+++ software/vision/project_outline_to_3d.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 X_SCALE = (21/22)
-Y_SCALE = (15/17)
+Y_SCALE = (2/3)
 
 def project_outline_to_3d(outline_points, camera_orig_dist, disp, depth, angle, width, height, vert_fov=85, hor_fov=128):
     
