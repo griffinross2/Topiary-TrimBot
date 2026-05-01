46e2763032e53b05bc030dc62751d068559db7ee
Griffin Ross <griffinross2@gmail.com>
2026-05-01 05:51:55 -0400

    More fun

--- firmware/count_lines.py
+++ firmware/count_lines.py
@@ -1,18 +1,32 @@
 import os
 
+file_exclusions = [
+    'cobs',
+    'system_stm32h7xx_dualcore_bootcm7_cm4gated',
+]
+
+dir_exclusions = [
+    'MarlinTT',
+    'fonts',
+    'graphics',
+    'images',
+    'tinyusb',
+    'fatfs'
+]
+
 def count_lines(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'rb') as file:
         return sum(1 for line in file)
     
-def traverse_directory(directory):
+def traverse_directory(directory, exclude_libs=True):
     total_lines = 0
 
     for root, dirs, files in os.walk(directory):
-        if 'MarlinTT' in root or 'fonts' in root or 'graphics' in root or 'images' in root or 'tinyusb' in root or 'fatfs' in root:
+        if exclude_libs and any(exclusion in root for exclusion in dir_exclusions):
             continue
         
         for file in files:
-            if 'cobs' in file or 'system_stm32h7xx_dualcore_bootcm7_cm4gated' in file:
+            if exclude_libs and any(exclusion in file for exclusion in file_exclusions):
                 continue
             if file.endswith('.cpp') or file.endswith('.h') or file.endswith('.c'):
                 file_path = os.path.join(root, file)
@@ -25,4 +39,4 @@ def traverse_directory(directory):
 
 if __name__ == "__main__":
     directory = "."
-    traverse_directory(directory)
+    traverse_directory(directory, True)
