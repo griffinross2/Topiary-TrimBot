Fix slice rotation issue in plant scanning

software/toolpath/gcode_generation.py
@@ -12,11 +12,12 @@ DECS = 3
 PARKING_R = 300 # mm
 START_POINT = (0, -390, 500, 0) # mm, mm, mm, deg
 MIN_R_SLOP = 5
-POT_HEIGHT = 254
-POT_DIAMETER = 280
+POT_HEIGHT = 260
+POT_DIAMETER = 300
 PLANT_KEEPOUT_DIAMETER = POT_DIAMETER * 0.75
 TOOLHEAD_LEN = 180
 MIN_ANGLE_CHANGE = 8 # degrees - if the angle change is less than this, don't bother changing the angle
+MIN_ANGLE = 15 # degrees - minimum angle of cutter
 
 # trimbot dimension constants
 D = 477.5
@@ -67,7 +68,7 @@ def get_trimmer_angle(curr_coord, next_coord):
     point_vec = np.subtract(next_coord, curr_coord)
     mag_point_vec = np.linalg.norm(point_vec)
     ang = np.arccos(np.dot(r_vec, point_vec) / mag_point_vec) * (180.0 / np.pi) - 90
-    ang = np.clip(ang, 0, 180)
+    ang = np.clip(ang, MIN_ANGLE, 180)
 
     # override the angle if the point is too close in to allow trimbot
     # to reach the point. This is since the extruder can only reach points

software/vision/project_outline_to_3d.py
@@ -10,12 +10,11 @@ def project_outline_to_3d(outline_points, camera_orig_dist, disp, depth, angle, 
     x_scale = X_SCALE * 2 * depth * np.tan(np.radians(hor_fov / 2)) / org_width
     y_scale = Y_SCALE * 2 * depth * np.tan(np.radians(vert_fov / 2)) / org_height
     r = camera_orig_dist - depth
+    theta = np.deg2rad(angle)
 
-    # pick basis vectors
-    n = np.array([-1 * np.cos(np.radians(angle)),-1 * np.sin(np.radians(angle)), 0])
-    u1 = (0, 0, 1)
-    u2 = np.cross(n, u1)
-    R = np.array([u2, u1, n]).T
+    p0 = np.array([r*np.cos(theta), r*np.sin(theta), 0.0])
+    x_axis = np.array([-np.sin(theta), np.cos(theta), 0.0])
+    y_axis = np.array([0.0, 0.0, 1.0])
 
     pts_3d = []
 
@@ -30,7 +29,7 @@ def project_outline_to_3d(outline_points, camera_orig_dist, disp, depth, angle, 
         y_inch = y_offset * y_scale
 
         # Transform this cross section by placing it on its plane in 3D
-        pt = np.matmul(R, np.array([x_inch, y_inch, 0])) + (r * n)
+        pt = p0 + x_inch*x_axis + y_inch*y_axis
         
         ## CONDITION TO DECREASE POINT SAMPLING:
         # if(i % 10 == 0): 